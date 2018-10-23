f352b802b2d057cabb5d9e0917302a3a623d9eed
Pablo <pawel.stolka@gmail.com>
2018-10-23 23:32:27 +0200

    back to master

--- index.js
+++ index.js
@@ -28,10 +28,10 @@ let io = socketIo(server)
 io.on('connection', (socket) => {
     console.log('Connected client on port %s.', PORT);
 
-    // socket.on('message', (m) => {
-    //     console.log('[server](message): %s', JSON.stringify(m));
-    //     io.emit('message', m);
-    // });
+    socket.on('message', (m) => {
+        console.log('[server](message): %s')//, JSON.stringify(m));
+        io.emit('message', m);
+    });
 
     socket.on('disconnect', () => console.log('Client disconnected...'));
 })
